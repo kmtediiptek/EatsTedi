27c8a1a1ba329112e91876f3abec0159c80a3c32
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2023-09-26 20:33:40 +0700

    added bell

--- resources/js/Components/OrderItem.jsx
+++ resources/js/Components/OrderItem.jsx
@@ -1,11 +1,19 @@
-import { IconChecks, IconCircleFilled, IconProgress } from '@tabler/icons-react';
-import React from 'react';
+import React from 'react'
+import { IconBell, IconChecks, IconCircleFilled, IconProgress } from '@tabler/icons-react'
+import ActionButton from './Actionbutton'
 
 export default function OrderItem({ invoice }) {
 
+    const callVoice = (name, table) => {
+        const speech = new SpeechSynthesisUtterance()
+        speech.text = `Order on Name ${name} on Table ${table}`
+        window.speechSynthesis.speak(speech)
+    }
+
     return (
-        <div className="flex gap-x-4 p-2 border border-gray-300 rounded text-white">
-            <div className="w-16 h-16 bg-sky-500 rounded p-2 flex justify-center items-center">
+        <div className="flex gap-x-4 p-2 border border-gray-300 rounded text-white relative" >
+            <ActionButton type="button" onClick={() => callVoice(invoice.name, invoice.table_id)} className='bg-purple-500/80 absolute top-[0px] left-[0px] z-[9999]'><IconBell size={18} /></ActionButton>
+            <div className="w-20 h-20 bg-sky-500 rounded p-2 flex justify-center items-center">
                 <h3 className='text-4xl font-semibold text-center'>T{invoice.table_id}</h3>
             </div>
             <div className="flex">
@@ -25,5 +33,5 @@ export default function OrderItem({ invoice }) {
                 </div>
             </div>
         </div>
-    );
+    )
 }
