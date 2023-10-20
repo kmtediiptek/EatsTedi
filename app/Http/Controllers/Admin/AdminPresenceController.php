b2fd0985ca2d7d036acefe359269ee6ae8de6d69
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2023-10-21 00:56:22 +0700

    change timezome

--- app/Http/Controllers/Admin/AdminPresenceController.php
+++ app/Http/Controllers/Admin/AdminPresenceController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\Admin\AdminPresenceResource;
 use App\Models\Presence;
 use App\Models\Salary;
-use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
