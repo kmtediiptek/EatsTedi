054722e6b5ac88f9e7b916dc64c0f46f33713fac
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2023-11-30 12:50:50 +0700

    build

---
+++ public/build/assets/Actionbutton-61e1fa01.js
@@ -0,0 +1,1 @@
+import{a as u}from"./app-50127253.js";function c({className:e="w-10 h-10 ",disabled:t,children:n,type:o=button,...r}){return u("button",{...r,type:o,className:`flex justify-center items-center bg-orange-500 text-white rounded ${t&&"opacity-25"} `+e,disabled:t,children:n})}export{c as A};
---
+++ public/build/assets/Textarea-7764c47a.js
@@ -0,0 +1,1 @@
+import{a as o}from"./app-50127253.js";function e({...r}){return o("textarea",{className:"border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full",...r})}export{e as T};
