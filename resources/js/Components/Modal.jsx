5f24cba8753be48cc28dd7bace8aed8fdb0c603e
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2024-07-31 02:43:05 +0700

    feat: fix modal

--- resources/js/Components/Modal.jsx
+++ resources/js/Components/Modal.jsx
@@ -1,12 +1,15 @@
-import { Dialog, Transition } from '@headlessui/react'
-import { IconX } from '@tabler/icons-react'
-import { Children, Fragment, useState } from 'react'
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
-export default function MyModal({ isOpen, onClose, size, title, children, type }) {
+export default function MyModal({ isOpen, setIsOpen, size, title, children, type }) {
+    function closeModal() {
+        setIsOpen(false);
+    }
+
     return (
         <>
             <Transition appear show={isOpen} as={Fragment}>
-                <Dialog as="div" className="relative z-10" onClose={onClose}>
+                <Dialog as="div" className="relative z-10" onClose={closeModal}>
                     <Transition.Child
                         as={Fragment}
                         enter="ease-out duration-300"
@@ -30,18 +33,22 @@ export default function MyModal({ isOpen, onClose, size, title, children, type }
                                 leaveFrom="opacity-100 scale-100"
                                 leaveTo="opacity-0 scale-95"
                             >
-                                <Dialog.Panel className={`w-full sm:w-${size} transform overflow-hidden rounded bg-white mt-16 p-6 text-left align-middle shadow-xl transition-all`}>
+                                <Dialog.Panel
+                                    className={`w-full sm:w-${size} transform overflow-hidden rounded bg-white mt-16 p-6 text-left align-middle shadow-xl transition-all`}
+                                >
                                     <Dialog.Title
                                         as="h3"
-                                        className="text-lg text-slate-700 font-medium leading-6 mt-4 text-gray-700"
+                                        className="text-lg text-slate-700 font-medium leading-6 mt-4"
                                     >
-                                        Form {type == "create" ? "Create" : type === "delete" ? "Delete" : "Edit"} {title}
+                                        Form{" "}
+                                        {type === "create"
+                                            ? "Create"
+                                            : type === "delete"
+                                            ? "Delete"
+                                            : "Edit"}{" "}
+                                        {title}
                                     </Dialog.Title>
-                                    <button className='absolute right-2 top-2 py-2 px-2 bg-transparent text-slate-500' onClick={onClose}><IconX /></button>
-                                    <div className="mt-2">
-                                        {children}
-                                    </div>
-
+                                    <div className="mt-2">{children}</div>
                                 </Dialog.Panel>
                             </Transition.Child>
                         </div>
@@ -49,5 +56,5 @@ export default function MyModal({ isOpen, onClose, size, title, children, type }
                 </Dialog>
             </Transition>
         </>
-    )
+    );
 }
