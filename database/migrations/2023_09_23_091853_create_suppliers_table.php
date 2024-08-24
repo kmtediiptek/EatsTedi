668326f3b7326453670ff50596a69520338dc4fc
gnafhan <nafhanghifari@gmail.com>
2024-08-24 07:00:12 +0700

    add user and fix table

--- database/migrations/2023_09_23_091853_create_suppliers_table.php
+++ database/migrations/2023_09_23_091853_create_suppliers_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->string('number_phone')->unique();
+            $table->string('username');
+            $table->string('email');
+            $table->string('number_phone');
             $table->text('address');
             $table->boolean('is_active')->default(true);
             $table->string('picture')->nullable();
---
+++ database/seeders/EatstediUserSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EatstediUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $newOwner = User::create([
+            'username' => 'naila',
+            'name' => 'Naila',
+            'email' => 'snailaafn27@gmail.com',
+            'password' => bcrypt("OwnerEats135"),
+            'number_phone' => '087738438643',
+            'address' => 'Jogja',
+            'picture' => '',
+            'status' => 'owner',
+        ]);
+
+        $newOwner->assignRole('owner');
+
+        $newEmployee1 = User::create([
+            'username' => 'kasir1',
+            'name' => 'Kasir 1',
+            'email' => 'eatstedi.trpl.tech@gmail.com',
+            'password' => bcrypt("EatsKasir975"),
+            'number_phone' => '081234567890',
+            'address' => 'Jogja',
+            'picture' => '',
+            'status' => 'employee',
+        ]);
+
+        $newEmployee1->assignRole('employee');
+
+        $newEmployee2 = User::create([
+            'username' => 'kasir2',
+            'name' => 'Kasir 2',
+            'email' => 'eatstedi.trpl.tech@gmail.com',
+            'password' => bcrypt("EatsKasir975"),
+            'number_phone' => '081234567890',
+            'address' => 'Jogja',
+            'picture' => '',
+            'status' => 'employee',
+        ]);
+
+        $newEmployee2->assignRole('employee');
+    }
+}
